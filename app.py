3352ce9257034e22becd53d64b6408e03bd9ecca
Alan <352469@niuitmo.ru>
2023-02-19 13:34:09 +0300

    db problem

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 from flask_shop.setup import app,api
 from flask_shop.model.Controler_Product import Controler_Product
+from flask_shop.model.db import db
+
+
 api.add_resource(Controler_Product, '/api/product')
 @app.route('/')
 def index():
-    return "tipo front"
+    return '''
+    <form action="/api/product" method="post" class="form-contact">
+<p><label>title: </label> <input type="text" name="title" value="" requied />
+<p><label>description: </label> <input type="text" name="description" value="" requied />
+<p><label>type: </label> <input type="text" name="type" value="" requied />
+
+<p><input type="submit" value="test" />
+</form>
+    '''
 if __name__ == "__main__":
     app.run(debug=True)
--- flask_shop/model/Controler_Product.py
+++ flask_shop/model/Controler_Product.py
@@ -1,8 +1,9 @@
 
 
 from flask_restful import Resource
-
+from flask_shop.model.db import db
 from .Products import Product
+from flask import request
 
 class Controler_Product(Resource):
     def get(self):
@@ -11,9 +12,20 @@ class Controler_Product(Resource):
         for i in info:
             s = str(i.id) + " name " + str(i.title)
             d = str(i.description)
-            data[i.id] = (s,d)
+            t = str(i.type)
+            data[i.id] = (s,d,t)
 
         return { "data": data }
     def post(self):
-        pass
+        try:
+            p = Product(title=request.form['title'], description=request.form['description'], type=request.form['type'])
+            db.session.add(p)
+            db.session.flush()
+            db.session.commit()
+            return {"result": "problem"}
+        except:
+            db.session.rollback()
+            return {"result": "problem"}
+
+
 
