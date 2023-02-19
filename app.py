23ed6343a137a974a096d4dcb2288131ebc9b15b
Alan <352469@niuitmo.ru>
2023-02-19 13:06:41 +0300

    model update

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from flask_shop.setup import app,api
-from flask_shop.model.test_resource import Users_
-api.add_resource(Users_, '/')
-
+from flask_shop.model.Controler_Product import Controler_Product
+api.add_resource(Controler_Product, '/api/product')
+@app.route('/')
+def index():
+    return "tipo front"
 if __name__ == "__main__":
     app.run(debug=True)
--- flask_shop/model/Products.py
+++ flask_shop/model/Products.py
@@ -6,10 +6,12 @@ from flask_shop.model.db import db
 
 
 class Product(db.Model):
+    __tablename__ = 'product'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=True)
-    price = db.Column(db.DECIMAL(10, 2))
     description = db.Column(db.Text())
+    type = db.Column(db.String(100), db.ForeignKey("type.name"))
+
 
     def __str__(self):
         return self.title
@@ -21,5 +23,9 @@ class Product(db.Model):
         return f"<users {self.id, self.name}"
 
 
+
    
     
+class Type(db.Model):
+    __tablename__ = 'type'
+    name = db.Column(db.String(100), primary_key=True)
